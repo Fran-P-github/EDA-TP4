dd72f3ec4ce4547906f212910e31a59888e4e705
Juan Ignacio Fogolin <juanifogo@gmail.com>
2025-11-08 19:06:43 -0300

    update game module

--- algorithm.cpp
+++ algorithm.cpp
@@ -5,6 +5,8 @@ static HomeBotPlan computeAttackerPlan(GameState &game);
 
 static HomeBotPlan computeDefenderPlan(GameState &game);
 
+static void assignRoles(GameState &game);
+
 TeamPlan computeTeamPlan(GameState &game) {
 	TeamPlan plan = {};
 	return plan;
@@ -16,4 +18,9 @@ static HomeBotPlan computeDefenderPlan(GameState &game) {
 
 static HomeBotPlan computeAttackerPlan(GameState &game) {
 
+}
+
+static void assignRoles(GameState &game) {
+	game.homeBot1.setRole(BotRole::ATTACKER);
+	game.homeBot2.setRole(BotRole::DEFENDER);
 }
